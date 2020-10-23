c9b3345ccd5e43a38ee9179269f6c30c33fdf56e
Abe <abhi.rawat456@gmail.com>
2020-10-23 17:17:22 +0530

    Showing Coaches using vuex

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,11 +4,26 @@
       <button>Refresh</button>
       <router-link to="/regsiter">Register as a Coach</router-link>
     </div>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
 </script>
 
 <style scoped>
