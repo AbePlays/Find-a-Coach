2d51568d8726d3e06a6e36ee290508b7c6f6167a
Abe <abhi.rawat456@gmail.com>
2020-10-23 18:34:12 +0530

    Added filtering

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,5 @@
 <template>
+  <coach-filter @change-filter="setFilters" />
   <section>
     <base-card>
       <div class="controls">
@@ -6,7 +7,7 @@
         <base-button link to="/regsiter">Register as a Coach</base-button>
       </div>
       <ul v-if="hasCoaches">
-        <CoachItem
+        <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
           :id="coach.id"
@@ -23,16 +24,45 @@
 
 <script>
 import CoachItem from "../../components/coaches/CoachItem.vue";
+import CoachFilter from "../../components/coaches/CoachFilter.vue";
+
 export default {
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
   components: {
     CoachItem,
+    CoachFilter,
   },
   computed: {
     filteredCoaches() {
-      return this.$store.getters["coaches/coaches"];
+      const coaches = this.$store.getters["coaches/coaches"];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes("frontend")) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes("backend")) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes("career")) {
+          return true;
+        }
+        return false;
+      });
     },
     hasCoaches() {
       return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
     },
   },
 };
