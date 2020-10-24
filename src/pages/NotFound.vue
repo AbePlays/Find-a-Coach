e1bdb2bc0beb15c68486ef5782ae92a890497a4c
Abe <abhi.rawat456@gmail.com>
2020-10-24 12:37:32 +0530

    Added Not Found Page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page you're looking for does not exist.</p>
+    </base-card>
+  </section>
 </template>
 
 <script>
