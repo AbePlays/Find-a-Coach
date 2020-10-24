e1939909794838c0413a4857f854162e63b766cc
Abe <abhi.rawat456@gmail.com>
2020-10-24 13:12:48 +0530

    Signup Page Setup

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import CoachesList from "./pages/coaches/CoachesList.vue";
 import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
 import ContactCoach from "./pages/requests/ContactCoach.vue";
 import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+import UserAuth from "./pages/auth/UserAuth.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -36,6 +37,7 @@ const router = createRouter({
       path: "/requests",
       component: RequestsReceived,
     },
+    { path: "/auth", component: UserAuth },
     {
       path: "/:notFound(.*)",
       component: NotFound,
