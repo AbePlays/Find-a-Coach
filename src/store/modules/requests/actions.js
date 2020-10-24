5367cd7fb0aafaec13bd8badec20e3b41383875e
Abe <abhi.rawat456@gmail.com>
2020-10-24 12:19:10 +0530

    Rendering Requests

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,13 +1,58 @@
 export default {
-  contactCoach(context, payload) {
-    console.log(payload);
+  async contactCoach(context, payload) {
     const req = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
 
+    const res = await fetch(
+      `https://xkcd-rn.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: "POST",
+        body: JSON.stringify(req),
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      const error = new Error(data.message || "Failed to fetch data!");
+      throw error;
+    }
+
+    req.id = data.name;
+    req.coachId = payload.coachId;
+
     context.commit("addRequest", req);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const res = await fetch(
+      `https://xkcd-rn.firebaseio.com/requests/${coachId}.json`
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      const error = new Error(data.message || "Failed to fetch data!");
+      throw error;
+    }
+
+    var requests = [];
+
+    for (var key in data) {
+      const req = {
+        id: key,
+        coachId: coachId,
+        userEmail: data[key].userEmail,
+        message: data[key].message,
+      };
+
+      requests.push(req);
+    }
+
+    console.log(requests);
+
+    context.commit("setRequests", requests);
   },
 };
